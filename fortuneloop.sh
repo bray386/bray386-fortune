9fa141c79edd6956bff1c908341f8380f0382b45
bray386 <62066434+bray386@users.noreply.github.com>
2020-04-02 09:15:55 -0500

    Update fortuneloop.sh

--- fortuneloop.sh
+++ fortuneloop.sh
@@ -1,5 +1,12 @@
-FROM ubuntu:latest
-RUN apt-get update ; apt-get -y install fortune
-ADD fortuneloop.sh /bin/fortuneloop.sh
-ENTRYPOINT ["/bin/fortuneloop.sh"]
-CMD ["10"]
+#!/bin/bash
+trap "exit" SIGINT
+INTERVAL=$1
+echo Configured to generate new fortune every $INTERVAL seconds
+mkdir /var/htdocs
+
+while :
+do
+  echo $(date) Writing fortune to /var/htdocs/index.html
+  /usr/games/fortune > /var/htdocs/index.html
+  sleep $INTERVAL
+done
